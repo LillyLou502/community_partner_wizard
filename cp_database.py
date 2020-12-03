afbf854c20349bbea2bf2e061fca48294d6b051e
Lilly Massa-McKinley <massamckinley@gmail.com>
2020-12-02 20:04:14 -0500

    created command function and first draft of create, read, update, delete functions

--- cp_database.py
+++ cp_database.py
@@ -1,10 +1,63 @@
 # get the CP database
+import csv
 import re
 import os
 
 class CommunityPartner():
     # create a dictionary inside the class
-    def __init__(self, org, contact, phone, email, web, primary_issue, special_issue):
+    def __init__(self):
+        pass
+
+    @property
+    def CP_dict(self):
+        return {"Organization": self.org,
+                "Contact Name": self.contact,
+                "Phone Number": self.phone,
+                "Email Address": self.email,
+                "Web Address": self.web,
+                "Primary Issue": self.primary_issue,
+                "Special Issue": self.special_issue}
+
+    def command(self):
+        command = input("What do you want to do in the Community Partner Database? Choose: create, read, update, or delete:   ")
+        if command == "create":
+            org = input("Organization Name:  ")
+            contact = input("Contact Name:  ")
+            phone = input("Contact Phone:  ")
+            email = input("Contact Email:  ")
+            web = input("Org Web Address:  ")
+            primary_issue = input("What is your Primary Social Issue " + 
+            "(Options: Education, Health/Wellness, Financial Empowerment, Environment, Social Services:  ")
+            special_issue = input("What special issue does your org address?  ")
+            self.create(org, contact, phone, email, web, primary_issue, special_issue)
+        if command == "read":
+            search = input("Would you like to search by organization name or primary social issue? Choose: org or issue  ")
+            if org:
+                org = input("Organization Name:  ")
+            if issue:
+                primary_issue = input("Primary Social Issue " + 
+                "(Options: Education, Health/Wellness, Financial Empowerment, Environment, Social Services:  ")
+            self.read(org, contact, phone, email, web, primary_issue, special_issue)
+        if command == "update":
+            update_org = input("What organization would you like to update?  ")
+            if update_org != org:
+                print("That organization is not in the database. Select 'create' to add it.")
+            if update_org == org:
+                org = input("Organization Name:  ")    
+                contact = input("Contact Name:  ")
+                phone = input("Contact Phone:  ")
+                email = input("Contact Email:  ")
+                web = input("Org Web Address:  ")
+                primary_issue = input("What is your Primary Social Issue " + 
+                "(Options: Education, Health/Wellness, Financial Empowerment, Environment, Social Services:  ")
+                special_issue = input("What special issue does your org address?  ")
+            self.update(org, contact, phone, email, web, primary_issue, special_issue)
+        if command == "delete":
+            org = input("Organization Name:  ")
+            self.delete(org)
+
+    def create(self, org, contact, phone, email, web, primary_issue, special_issue):
+        # allow user to add to CP
         self.org = org
         self.contact = contact
         self.phone = phone
@@ -13,24 +66,35 @@ class CommunityPartner():
         self.primary_issue = primary_issue
         self.special_issue = special_issue
 
-@property
-def CP_dict(self):
-    return {"Organization": self.org,
-            "Contac Name": self.contact,
-            "Phone Number": self.phone,
-            "Email Address": self.email,
-            "Web Address": self.web,
-            "Select Primary Social Justice Issues (check all that apply)": self.primary_issue,
-            "In your own words, describe the social issues your organization addresses:": self.special_issue}
+    def read(self, org, primary_issue):
+        # allow user to look up a CP by org or issue
+        # not sure how to call the rest of the values associated with this org or issue key
+        self.org = org
+        self.primary_issue = primary_issue
 
-def create():
-    # allow user to add to CP
+    def update(self, org, contact, phone, email, web, primary_issue, special_issue):
+        # allow user to change info in CP
+        # how do I make sure this updates instead of adding new?
+        self.org = org
+        self.contact = contact
+        self.phone = phone
+        self.email = email
+        self.web = web
+        self.primary_issue = primary_issue
+        self.special_issue = special_issue
 
-def read():
-    # allow user to look up a CP by org or issue
+    def delete(self, org):
+        # allow user to delete CP
+        print("Do you want to delete {} from the community partner database? yes or no:  ".format(org)):
+            if yes:
+                del self.org
+            else:
+                return command()
 
-def update():
-    # allow user to change info in CP
+    # after each command is executed, print stats of database
+    # number of community partners in the database
+    # number of orgs for each primary issue
 
-def delete():
-    # allow user to delete CP
+if __name__ == "__main__":
+    partner = CommunityPartner()
+    partner.command()
