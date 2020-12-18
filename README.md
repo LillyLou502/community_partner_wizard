1a30531b41e0a8f8d75121084b950ebbc24a2762
Lilly Massa-McKinley <massamckinley@gmail.com>
2020-12-18 17:01:22 -0500

    Added comments in project

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ For this project, the features I chose to include are:
 2) create a class, then create at least one object of that class and populate it with data, 
 3) create a dictionary or list, populate it with several values, retrieve at least one value, and use it in your program, 
 4) create and call at least 3 functions, at least one of which must return a value that is used.
+5) analyze text and display information about it (ex: how many words in a paragraph)
 ## special_instructions
 While I had a larger and more complex vision for this project when I first began, I needed to simplify it as my skill set is still in the beginning level. I did not use regular expressions in this database to verify correct entries for phone numbers and emails. Through the command function, users will be prompted with specific key words to use to continue in the program and these key words are case sensitive and must be typed exactly as they appear. Duplicate entries of community partners are currently allowed. I realize there are many features that would enhance this basic program, and I hope to be able to add them in the future as I continue learning Python.
--- cp_database.py
+++ cp_database.py
@@ -1,5 +1,4 @@
-
-
+# CommunityPartner class stores a representation of a community partner.
 class CommunityPartner():
     def __init__(self, org, contact, phone, email, issue):
         self.org = org
@@ -8,6 +7,7 @@ class CommunityPartner():
         self.email = email
         self.issue = issue
 
+# The database includes a sample list of community organizations that the user can read, update, delete, or add to.
 database = [CommunityPartner("Metro United Way", "Jamie Miller", "502-550-5567", "jamie@muw.org", "Social Services"), 
             CommunityPartner("Boys and Girls Club", "Kathy Jones", "502-123-6789", "kathy@bgclub.org", "Education"),
             CommunityPartner("Earth and Spirit Center", "Bobby Hackett", "819-543-6543", "bobby@esc.org", "Environment"),
@@ -18,18 +18,22 @@ database = [CommunityPartner("Metro United Way", "Jamie Miller", "502-550-5567",
             CommunityPartner("Thrive Center", "Rosy Smith", "502-675-0987", "rsmith@thrive.org", "Health/Wellness")
 ]
 
+# Created issue_options to easily update issue options in the future.
 issue_options = "Options: Education, Health/Wellness, Environment, Social Services"
 
+# Allows the user to add a new community partner to the database.
 def create(org, contact, phone, email, issue):
     database.append(CommunityPartner(org, contact, phone, email, issue))
     print("There are {} organizations in the Community Parnter Database.".format(len(database)))
     command()
 
+# Allows the user to read all entries in the database.
 def read_all():
     for item in database:
         print(item.org, item.contact, item.phone, item.email, item.issue, sep = ", ")
     command()
 
+# Allows the user to read community partners with a common social issue.
 def read_by_issue(issue):
     match = 0
     for item in database:
@@ -40,12 +44,14 @@ def read_by_issue(issue):
         print("There are no organizations with that social issue in the database.")
     command()
 
+# This function is only used when a user wants to update or delete an organization from the database.
 def read_by_org(org):
     for i, item in enumerate(database):
         if item.org == org:
             return i
     print("That organization is not in the database.")
 
+# This function deletes an organization from the database.
 def delete(org):
     org_index = read_by_org(org)
     if org_index == None:
@@ -53,8 +59,11 @@ def delete(org):
     else:
         database.pop(org_index)
 
+# The command function starts and ends the program by asking users how they would like to interact with the database.
+# Users will continue looping through the command function until they type 'exit'.
 def command():
     options = input("What do you want to do in the Community Partner Database? Choose: create, read, update, or delete. Type 'exit' to leave the program.   ")
+    # The 'create' operation allows users to add a new community partner to the database.
     if options == "create":
         org = input("Organization Name:  ")
         contact = input("Contact Name:  ")
@@ -62,12 +71,14 @@ def command():
         email = input("Contact Email:  ")
         issue = input("What is the Social Issue " + issue_options + ":  ")
         create(org, contact, phone, email, issue)
+    # The 'read' operation allows users to read all entries in the database or read all organizations addressing a specific community issue.
     if options == "read":
         read_choice = input("Do you want to read all entries or sort by or or issue? Type 'all' or 'issue':  ")
         if read_choice == "all":
             read_all()
         if read_choice == "issue":
             read_by_issue(input("What issue would you like to search? " + issue_options + ":  "))
+    # The 'update' operation first deletes the old entry for an organization and then allows the user to reenter updated information.
     if options == "update":
         org = input("What organization information would you like to update?  ")
         delete(org)
@@ -77,6 +88,7 @@ def command():
         email = input("Contact Email:  ")
         issue = input("What is the Social Issue " + issue_options + ":  ")
         create(org, contact, phone, email, issue)
+    # The 'delete' operation allows users to delete an entry from a database. It also confirms the user wants to delete before executing the function.
     if options == "delete":
         org = input("What organization would you like to delete?  ")
         del_confirm = input("Do you want to delete {} from the community partner database? yes or no:  ".format(org))
@@ -85,9 +97,11 @@ def command():
             command()
         else:
             command()
+    # The 'exit' operation allows the user to exit the program.
     if options == "exit":
         pass
 
+# At the end of each operation inside the command function, the program will print the current number of community partners in the database.
 if __name__ == "__main__":
     command()
     print("There are {} organizations in the Community Parnter Database.".format(len(database)))
